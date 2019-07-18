34bfa2326c99dc0136c991d524bd384467d4edbb
Marcos Forlenza <mnforlenza@gmail.com>
2019-07-18 00:16:45 -0300

    Spring controller tests added

--- src/test/java/org/jumahuaca/examples/app/SpringExchangeRestControllerTests.java
+++ src/test/java/org/jumahuaca/examples/app/SpringExchangeRestControllerTests.java
@@ -13,9 +13,8 @@ import static org.jumahuaca.examples.controller.PathConstants.UVA_EXCHANGE_PUT_P
 import static org.jumahuaca.examples.controller.PathConstants.UVA_EXCHANGE_REMOVE_PATH;
 import static org.jumahuaca.examples.controller.PathConstants.UVA_EXCHANGE_ROOT_PATH;
 import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.doThrow;
-
+import static org.mockito.Mockito.when;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -28,23 +27,24 @@ import javax.servlet.http.HttpServletResponse;
 import org.jumahuaca.examples.controller.UVAExchangeController;
 import org.jumahuaca.examples.entity.UVAExchange;
 import org.jumahuaca.examples.repository.UvaExchangeRepository;
+import org.jumahuaca.extensions.SpringControllerCrudExtension;
+import org.jumahuaca.extensions.SpringControllerCrudExtensionHelper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(UVAExchangeController.class)
-public class SpringExchangeRestControllerTests {
+public class SpringExchangeRestControllerTests implements SpringControllerCrudExtensionHelper<UVAExchange>{
 
 	private static final String PREFIX = RESOURCE_VERSION + UVA_EXCHANGE_ROOT_PATH;
 
@@ -56,6 +56,9 @@ public class SpringExchangeRestControllerTests {
 
 	@Autowired
 	private ObjectMapper objectMapper;
+	
+	@RegisterExtension
+	public final SpringControllerCrudExtension<UVAExchange> extension = new SpringControllerCrudExtension<UVAExchange>();
 
 	private String testDay1 = "2016-03-31";
 
@@ -78,137 +81,101 @@ public class SpringExchangeRestControllerTests {
 	@Test
 	public void requestAllUVAExchangeShouldWork() throws Exception {
 		List<UVAExchange> mockedResult = mockSelectAllResult();
-		stubRepositoryFindAllOk(mockedResult);
-		MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get(PREFIX + "/" + UVA_EXCHANGE_GET_ALL_PATH)
-				.accept(MediaType.APPLICATION_JSON_UTF8)).andReturn();
+		MvcResult result  = extension.requestAllShouldWork(this, mockMvc, PREFIX + "/" + UVA_EXCHANGE_GET_ALL_PATH);
 		List<UVAExchange> requestResult = objectMapper.readValue(result.getResponse().getContentAsString(),
-				new TypeReference<List<UVAExchange>>() {
-				});
+		new TypeReference<List<UVAExchange>>() {
+		});
 		HttpServletResponse response = result.getResponse();
 		assertResponse200(response);
 		assertThat(requestResult).isEqualTo(mockedResult);
 	}
-
+	
 	@Test
 	public void requestAllUVAExchangeShouldNotReturn() throws Exception {
-		stubRepositoryFindAllNotFound();
-		MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get(PREFIX + "/" + UVA_EXCHANGE_GET_ALL_PATH)
-				.accept(MediaType.APPLICATION_JSON_UTF8)).andReturn();
+		MvcResult result  = extension.requestAllShouldNotReturn(this, mockMvc, PREFIX + "/" + UVA_EXCHANGE_GET_ALL_PATH);
 		HttpServletResponse response = result.getResponse();
 		assertResponse404(response);
 	}
-
+	
 	@Test
 	public void requestAllUVAExchangeShouldFail() throws Exception {
-		stubRepositoryFindAllError();
-		MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get(PREFIX + "/" + UVA_EXCHANGE_GET_ALL_PATH)
-				.accept(MediaType.APPLICATION_JSON_UTF8)).andReturn();
+		MvcResult result  = extension.requestAllShouldFail(this, mockMvc, PREFIX + "/" + UVA_EXCHANGE_GET_ALL_PATH);
 		HttpServletResponse response = result.getResponse();
 		assertResponse500(response);
 	}
-
+	
 	@Test
 	public void requestOneUVAExchangeShouldWork() throws Exception {
-		UVAExchange mockedResult = mockOneUVAExchange();
-		stubRepositoryFindByIdOk(mockedResult);
-		MvcResult result = mockMvc
-				.perform(
-						MockMvcRequestBuilders
-								.get(PREFIX + UVA_EXCHANGE_GET_ONE_PATH + UVA_EXCHANGE_GET_ONE_PARAMS,
-										testDay1YearParam, testDay1MonthParam, testDay1DayParam)
-								.accept(MediaType.APPLICATION_JSON_UTF8))
-				.andReturn();
+		UVAExchange mockedResult = mockOne();
+		MvcResult result  = extension.requestOneShouldWork(this, mockMvc, PREFIX + UVA_EXCHANGE_GET_ONE_PATH + UVA_EXCHANGE_GET_ONE_PARAMS,
+				testDay1YearParam, testDay1MonthParam, testDay1DayParam);
+				
 		UVAExchange requestResult = objectMapper.readValue(result.getResponse().getContentAsString(),
 				UVAExchange.class);
 		HttpServletResponse response = result.getResponse();
 		assertResponse200(response);
 		assertThat(requestResult).isEqualTo(mockedResult);
+		
 	}
 
 	@Test
 	public void requestOneUVAExchangeShouldNotFound() throws Exception {
-		UVAExchange mockedResult = mockOneUVAExchange();
-		stubRepositoryFindByIdNotFound(mockedResult);
-		MvcResult result = mockMvc
-				.perform(
-						MockMvcRequestBuilders
-								.get(PREFIX + UVA_EXCHANGE_GET_ONE_PATH + UVA_EXCHANGE_GET_ONE_PARAMS,
-										testDay1YearParam, testDay1MonthParam, testDay1DayParam)
-								.accept(MediaType.APPLICATION_JSON_UTF8))
-				.andReturn();
+		MvcResult result  = extension.requestOneShouldNotFound(this, mockMvc, PREFIX + UVA_EXCHANGE_GET_ONE_PATH + UVA_EXCHANGE_GET_ONE_PARAMS,
+				testDay1YearParam, testDay1MonthParam, testDay1DayParam);
 		HttpServletResponse response = result.getResponse();
 		assertResponse404(response);
 	}
-
+	
 	@Test
 	public void postUVAExchangeShouldWork() throws Exception {
-		UVAExchange mockedResult = mockOneUVAExchange();
-		stubRepositoryUpdateOk(mockedResult);
-		MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post(PREFIX + UVA_EXCHANGE_POST_PATH)
-				.contentType(MediaType.APPLICATION_JSON_UTF8).accept(MediaType.APPLICATION_JSON_UTF8)
-				.content(objectMapper.writeValueAsString(mockedResult))).andReturn();
+		UVAExchange mockedResult = mockOne();
+		MvcResult result = extension.postShouldWork(this, mockMvc, PREFIX + UVA_EXCHANGE_POST_PATH, objectMapper.writeValueAsString(mockedResult));
 		HttpServletResponse response = result.getResponse();
 		assertResponse200(response);
 	}
-
+	
 	@Test
-	public void postUVAExchangeShouldFail() throws Exception {
-		UVAExchange mockedResult = mockOneUVAExchange();
-		stubRepositoryUpdateError(mockedResult);
-		MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post(PREFIX + UVA_EXCHANGE_POST_PATH)
-				.contentType(MediaType.APPLICATION_JSON_UTF8).accept(MediaType.APPLICATION_JSON_UTF8)
-				.content(objectMapper.writeValueAsString(mockedResult))).andReturn();
+	public void postShouldFail() throws Exception {
+		UVAExchange mockedResult = mockOne();
+		MvcResult result = extension.postShouldFail(this, mockMvc, PREFIX + UVA_EXCHANGE_POST_PATH, objectMapper.writeValueAsString(mockedResult));
 		HttpServletResponse response = result.getResponse();
 		assertResponse500(response);
 	}
-
+	
 	@Test
 	public void putUVAExchangeShouldWork() throws Exception {
-		UVAExchange mockedResult = mockOneUVAExchange();
-		stubRepositoryUpdateOk(mockedResult);
-		MvcResult result = mockMvc.perform(
-				MockMvcRequestBuilders.put(PREFIX + UVA_EXCHANGE_PUT_PATH).contentType(MediaType.APPLICATION_JSON_UTF8)
-						.accept(MediaType.APPLICATION_JSON_UTF8).content(objectMapper.writeValueAsString(mockedResult)))
-				.andReturn();
+		UVAExchange mockedResult = mockOne();
+		MvcResult result = extension.putShouldWork(this, mockMvc, PREFIX + UVA_EXCHANGE_PUT_PATH, objectMapper.writeValueAsString(mockedResult));
 		HttpServletResponse response = result.getResponse();
 		assertResponse200(response);
 	}
-
+	
 	@Test
 	public void putUVAExchangeShouldFail() throws Exception {
-		UVAExchange mockedResult = mockOneUVAExchange();
-		stubRepositoryUpdateError(mockedResult);
-		MvcResult result = mockMvc.perform(
-				MockMvcRequestBuilders.put(PREFIX + UVA_EXCHANGE_PUT_PATH).contentType(MediaType.APPLICATION_JSON_UTF8)
-						.accept(MediaType.APPLICATION_JSON_UTF8).content(objectMapper.writeValueAsString(mockedResult)))
-				.andReturn();
+		UVAExchange mockedResult = mockOne();
+		MvcResult result = extension.putShouldFail(this, mockMvc, PREFIX + UVA_EXCHANGE_PUT_PATH, objectMapper.writeValueAsString(mockedResult));
 		HttpServletResponse response = result.getResponse();
 		assertResponse500(response);
 	}
-
+	
 	@Test
 	public void deleteUVAExchangeShouldWork() throws Exception {
-		UVAExchange mockedResult = mockOneUVAExchange();
-		stubRepositoryDeleteOk(mockedResult);
-		MvcResult result = mockMvc.perform(MockMvcRequestBuilders.delete(PREFIX + UVA_EXCHANGE_REMOVE_PATH)
-				.contentType(MediaType.APPLICATION_JSON_UTF8).accept(MediaType.APPLICATION_JSON_UTF8)
-				.content(objectMapper.writeValueAsString(mockedResult))).andReturn();
+		UVAExchange mockedResult = mockOne();
+		MvcResult result = extension.putShouldWork(this, mockMvc, PREFIX + UVA_EXCHANGE_REMOVE_PATH, objectMapper.writeValueAsString(mockedResult));
 		HttpServletResponse response = result.getResponse();
 		assertResponse200(response);
 	}
-
+	
 	@Test
 	public void deleteUVAExchangeShouldFail() throws Exception {
-		UVAExchange mockedResult = mockOneUVAExchange();
-		stubRepositoryDeleteError(mockedResult);
-		MvcResult result = mockMvc.perform(MockMvcRequestBuilders.delete(PREFIX + UVA_EXCHANGE_REMOVE_PATH)
-				.contentType(MediaType.APPLICATION_JSON_UTF8).accept(MediaType.APPLICATION_JSON_UTF8)
-				.content(objectMapper.writeValueAsString(mockedResult))).andReturn();
+		UVAExchange mockedResult = mockOne();
+		MvcResult result = extension.putShouldFail(this, mockMvc, PREFIX + UVA_EXCHANGE_REMOVE_PATH, objectMapper.writeValueAsString(mockedResult));
 		HttpServletResponse response = result.getResponse();
 		assertResponse500(response);
 	}
 
-	private List<UVAExchange> mockSelectAllResult() {
+	@Override
+	public List<UVAExchange> mockSelectAllResult() {
 		List<UVAExchange> result = new ArrayList<UVAExchange>();
 		result.add(new UVAExchange(LocalDate.parse(testDay1), BigDecimal.valueOf(testRate1)));
 		result.add(new UVAExchange(LocalDate.parse(testDay2), BigDecimal.valueOf(testRate2)));
@@ -216,46 +183,56 @@ public class SpringExchangeRestControllerTests {
 		return result;
 	}
 
-	private void stubRepositoryFindAllOk(List<UVAExchange> mockedResult) {
+	@Override
+	public void stubRepositoryFindAllOk(List<UVAExchange> mockedResult) {
 		when(uvaExchangeRepository.findAll()).thenReturn(mockedResult);
 	}
 
-	private void stubRepositoryFindAllNotFound() {
+	@Override
+	public void stubRepositoryFindAllNotFound() {
 		List<UVAExchange> emptyResult = new ArrayList<UVAExchange>();
 		when(uvaExchangeRepository.findAll()).thenReturn(emptyResult);
 	}
 
-	private void stubRepositoryFindAllError() {
+	@Override
+	public void stubRepositoryFindAllError() {
 		List<UVAExchange> emptyResult = null;
 		when(uvaExchangeRepository.findAll()).thenReturn(emptyResult);
 	}
 
-	private void stubRepositoryFindByIdOk(UVAExchange mockedResult) {
+	@Override
+	public void stubRepositoryFindByIdOk(UVAExchange mockedResult) {
 		when(uvaExchangeRepository.findById(mockedResult.getDate())).thenReturn(Optional.of(mockedResult));
 	}
 
-	private void stubRepositoryFindByIdNotFound(UVAExchange mockedResult) {
+	@Override
+	public void stubRepositoryFindByIdNotFound(UVAExchange mockedResult) {
 		Optional<UVAExchange> nullResult = Optional.ofNullable(null);
 		when(uvaExchangeRepository.findById(mockedResult.getDate())).thenReturn(nullResult);
 	}
 
-	private UVAExchange mockOneUVAExchange() {
+	@Override
+	public UVAExchange mockOne() {
 		return new UVAExchange(LocalDate.parse(testDay1), BigDecimal.valueOf(testRate1));
 	}
 
-	private void stubRepositoryUpdateOk(UVAExchange mockedResult) {
+	@Override
+	public void stubRepositoryUpdateOk(UVAExchange mockedResult) {
 		when(uvaExchangeRepository.save(mockedResult)).thenReturn(mockedResult);
 	}
 
-	private void stubRepositoryUpdateError(UVAExchange mockedResult) {
+	@Override
+	public void stubRepositoryUpdateError(UVAExchange mockedResult) {
 		when(uvaExchangeRepository.save(mockedResult)).thenThrow(RuntimeException.class);
 	}
 
-	private void stubRepositoryDeleteOk(UVAExchange mockedResult) {
+	@Override
+	public void stubRepositoryDeleteOk(UVAExchange mockedResult) {
 		doNothing().when(uvaExchangeRepository).delete(mockedResult);
 	}
 
-	private void stubRepositoryDeleteError(UVAExchange mockedResult) {
+	@Override
+	public void stubRepositoryDeleteError(UVAExchange mockedResult) {
 		doThrow(RuntimeException.class).when(uvaExchangeRepository).delete(mockedResult);
 	}
 
